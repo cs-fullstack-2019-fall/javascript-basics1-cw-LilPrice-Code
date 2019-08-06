aef63fd048f7aed9c1e974e17d7cf7db2f676e20
LilPrice-Code <littleprice002@gmail.com>
2019-08-06 15:48:09 -0500

    Completed all Problems

---
+++ INDEX.js
@@ -0,0 +1,15 @@
+// Problem 1 Print "Hello World"
+//
+// Problem 2 Create a variable called nameVariable. Print "Hello " + nameVariable
+//
+// Problem 3 Create a variable named dogAge. Print "Your dog is: " + dogAge * 7
+//
+// Problem 4 Print "1 + 1 = " and the answer.
+
+alert("Hello World");
+var nameVariable = "John";
+console.log("Hello " + nameVariable);
+var dogAge = 4;
+console.log("Your dog is " + dogAge * 7);
+var answer = 2;
+console.log("1 + 1 = " + answer);
